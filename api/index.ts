48c7eabf35f3229458a185d47dbdeaaf93df4e74
abdulbasit12m <abdulbasit12m@gmail.com>
2026-09-11 21:21:34 +0500

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { Request, Response } from 'express';
-import { createApp } from '../src/bootstrap';
+import { AppModule, ObserveInstrument } from '../src/app.module';
 
-let appPromise: ReturnType<typeof createApp> | undefined;
+let appPromise: ReturnType<typeof createNestApp> | undefined;
+
+async function createNestApp() {
+  const app = await NestFactory.create(AppModule, {
+    instrument: ObserveInstrument,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.init();
+  return app;
+}
 
 export default async function handler(request: Request, response: Response) {
-  appPromise ??= createApp().then(async (app) => {
-    await app.init();
-    return app;
-  });
+  appPromise ??= createNestApp();
 
   const app = await appPromise;
   return app.getHttpAdapter().getInstance()(request, response);
