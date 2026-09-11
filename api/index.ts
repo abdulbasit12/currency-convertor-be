0efd6da33346f7b46fcf2344c7a86b29224fff3a
abdulbasit12m <abdulbasit12m@gmail.com>
2026-09-11 23:43:48 +0500

    test

--- api/index.ts
+++ api/index.ts
@@ -3,8 +3,6 @@ import 'reflect-metadata';
 import type { Request, Response } from 'express';
 import type { INestApplication } from '@nestjs/common';
 
-declare const require: NodeRequire;
-
 let appPromise: Promise<INestApplication> | undefined;
 
 async function getCurrencies(request: Request, response: Response) {
@@ -40,9 +38,11 @@ function removeApiPrefix(request: Request) {
 }
 
 async function createNestApp() {
-  const { ValidationPipe } = require('@nestjs/common') as typeof import('@nestjs/common');
-  const { NestFactory } = require('@nestjs/core') as typeof import('@nestjs/core');
-  const { AppModule } = require('../src/app.module') as typeof import('../src/app.module');
+  const [{ ValidationPipe }, { NestFactory }, { AppModule }] = await Promise.all([
+    import('@nestjs/common'),
+    import('@nestjs/core'),
+    import('../src/app.module.js'),
+  ]);
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
