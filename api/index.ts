e16c18ab2c4c1ddf18f3c2cfed4468865cc21a28
abdulbasit12m <abdulbasit12m@gmail.com>
2026-09-11 23:22:32 +0500

    test

--- api/index.ts
+++ api/index.ts
@@ -32,10 +32,6 @@ function getRequestPath(request: Request) {
   return pathname.replace(/^\/api(?=\/|$)/, '') || '/';
 }
 
-function removeApiPrefix(request: Request) {
-  request.url = request.url.replace(/^\/api(?=\/|$)/, '') || '/';
-}
-
 async function createNestApp() {
   const { ValidationPipe } = require('@nestjs/common') as typeof import('@nestjs/common');
   const { NestFactory } = require('@nestjs/core') as typeof import('@nestjs/core');
@@ -59,7 +55,6 @@ export default async function handler(request: Request, response: Response) {
   }
 
   try {
-    removeApiPrefix(request);
     appPromise ??= createNestApp();
     const app = await appPromise;
     return app.getHttpAdapter().getInstance()(request, response);
